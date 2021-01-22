95eba4aa873f9aa183f8d8cd96fda03be4d67293
Ze-Hao Wang (Salmon) <salmon.zh.tw@gmail.com>
2021-01-22 17:30:52 +0800

    implement index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,46 @@
 // Node modules.
+import _ from 'lodash';
+// Local modules.
+import pokemonList from '../data/pokemon-list.json';
+import formList from '../data/form-list.json';
 
-const main = async () => {
+type Locale = 'en-US' | 'zh-TW';
+
+const getPokemonByNo = (pokemonNo: number) => {
+  return pokemonList.find((pokemon) => pokemon.no === pokemonNo);
 };
 
-main();
+const getFormsByNo = (pokemonNo: number) => {
+  return formList.filter((pokemon) => pokemon.no === pokemonNo);
+};
+
+const transPokemonName = (pokemonName: string, pokemonNo: number, targetLocale: Locale = 'zh-TW') => {
+  const baseLocale: Locale = 'en-US';
+  const pokemon = getPokemonByNo(pokemonNo);
+  const forms = getFormsByNo(pokemonNo);
+
+  let translatedName = pokemonName;
+
+  if (pokemon) {
+    // Replace base name.
+    translatedName = translatedName.replace(
+      new RegExp(pokemon[baseLocale], 'i'),
+      pokemon[targetLocale],
+    );
+    // Replace form type.
+    forms.forEach((form) => {
+      if (form[baseLocale] && form[targetLocale]) {
+        translatedName = translatedName.replace(
+          new RegExp(form[baseLocale]!, 'i'),
+          form[targetLocale]!,
+        );
+      }
+    });
+  }
+
+  return translatedName;
+};
+
+export {
+  transPokemonName,
+};
